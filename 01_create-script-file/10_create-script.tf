8e28fa77782aad5d4e47e9a89900d7cef973127b
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2016-10-18 11:06:54 +0900

    [Update] テンプレートファイルが変更されたときもindex.jsを再生成する

--- 01_create-script-file/10_create-script.tf
+++ 01_create-script-file/10_create-script.tf
@@ -3,6 +3,7 @@ resource "null_resource" "create_index_js" {
 	triggers = {
 		target_region = "${var.target_region}"
 		slack_webhook_url = "${var.slack_webhook_url}"
+		templates_file = "${base64sha256(file("./templates/lambda_ec2-description-to-slack-using-https.js"))}"
 	}
 	provisioner "local-exec" {
 		command = "node create_index.js.js ${var.target_region} ${var.slack_webhook_url}"
