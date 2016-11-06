814a963f90f1d7bb5cdbddc7ae3b8f538c85c2f1
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2016-11-06 18:18:25 +0900

    [Clean] role-policyの定義を整理

--- 03_create-lambda-function/20_iam-role.tf
+++ 03_create-lambda-function/20_iam-role.tf
@@ -4,8 +4,14 @@ resource "aws_iam_role" "iam_for_lambda" {
 	assume_role_policy = "${file("./iam-role/assume_role_policy-trust_lambda.json")}"
 }
 
+resource "aws_iam_role_policy" "aws_lambda_basic_execution" {
+	name = "aws_lambda_basic_execution"
+	role = "${aws_iam_role.iam_for_lambda.id}"
+	policy = "${file("./iam-role/policy-aws_lambda_basic_execution.json")}"
+}
+
 resource "aws_iam_role_policy" "allow_access_ec2_description" {
-	name = "${var.prefix}-allow_access_ec2_description"
+	name = "allow_access_ec2_description"
 	role = "${aws_iam_role.iam_for_lambda.id}"
 	policy = "${file("./iam-role/policy-allow_access_ec2_description.json")}"
 }
