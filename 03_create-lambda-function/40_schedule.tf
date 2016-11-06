f810d116eca1d9c0aed38472b69a37c1dc6b6ac6
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2016-11-06 18:21:29 +0900

    [Clean] スケジュール resourceの名前整理

--- 03_create-lambda-function/40_schedule.tf
+++ 03_create-lambda-function/40_schedule.tf
@@ -1,11 +1,11 @@
 
-resource "aws_cloudwatch_event_rule" "every_hour" {
+resource "aws_cloudwatch_event_rule" "schedule" {
 	name = "${var.schedule_name}"
 	schedule_expression = "${var.schedule_expression}"
 }
 
-resource "aws_cloudwatch_event_target" "notification_every_hour" {
-	rule = "${aws_cloudwatch_event_rule.every_hour.name}"
+resource "aws_cloudwatch_event_target" "notification_schedule_target" {
+	rule = "${aws_cloudwatch_event_rule.schedule.name}"
 	arn = "${aws_lambda_function.notification.arn}"
 }
 
@@ -14,5 +14,5 @@ resource "aws_lambda_permission" "allow_cloudwatch_to_call_notification" {
 	action = "lambda:InvokeFunction"
 	function_name = "${aws_lambda_function.notification.function_name}"
 	principal = "events.amazonaws.com"
-	source_arn = "${aws_cloudwatch_event_rule.every_hour.arn}"
+	source_arn = "${aws_cloudwatch_event_rule.schedule.arn}"
 }
