dc1faac87494de20520eceab1f64e20a43b83883
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-11-03 22:12:28 +0900

    [Clean] Lambda実行用policyをインラインポリシーからAWS管理ポリシーに変更

--- 20_iam-role.tf
+++ 20_iam-role.tf
@@ -4,10 +4,9 @@ resource "aws_iam_role" "iam_for_lambda" {
 	assume_role_policy = "${file("./iam-role/assume_role_policy-trust_lambda.json")}"
 }
 
-resource "aws_iam_role_policy" "aws_lambda_basic_execution" {
-	name = "aws_lambda_basic_execution"
-	role = "${aws_iam_role.iam_for_lambda.id}"
-	policy = "${file("./iam-role/policy-aws_lambda_basic_execution.json")}"
+resource "aws_iam_role_policy_attachment" "aws_lambda_basic_execution_attach" {
+	role = "${aws_iam_role.iam_for_lambda.name}"
+	policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_iam_role_policy" "allow_access_ec2_description" {
