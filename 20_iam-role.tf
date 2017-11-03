3d0acf1531d8400c05c00c5876171f83ea983b91
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-11-03 22:20:29 +0900

    [Move] assume_role_policy と policy をフォルダ分け

--- 20_iam-role.tf
+++ 20_iam-role.tf
@@ -1,7 +1,7 @@
 
 resource "aws_iam_role" "iam_for_lambda" {
 	name = "${var.prefix}-iam_for_lambda"
-	assume_role_policy = "${file("./iam-role/assume_role_policy-trust_lambda.json")}"
+	assume_role_policy = "${file("./iam-role/assume_role_policy/trust_lambda.json")}"
 }
 
 resource "aws_iam_role_policy_attachment" "aws_lambda_basic_execution_attach" {
@@ -12,5 +12,5 @@ resource "aws_iam_role_policy_attachment" "aws_lambda_basic_execution_attach" {
 resource "aws_iam_role_policy" "allow_access_ec2_description" {
 	name = "allow_access_ec2_description"
 	role = "${aws_iam_role.iam_for_lambda.id}"
-	policy = "${file("./iam-role/policy-allow_access_ec2_description.json")}"
+	policy = "${file("./iam-role/policy/allow_access_ec2_description.json")}"
 }
